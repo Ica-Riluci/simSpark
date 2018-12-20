aecf481d8a7d27bc73f3c564b190b43df2998e34
nlosilva1 <thss15_yit@163.com>
2018-12-20 23:40:36 +0800

    Add a simple Sparkconn

---
+++ publib/SparkConn.py
@@ -0,0 +1,55 @@
+import socket
+import json
+
+class SparkConn:
+    __host__ = socket.gethostbyname(socket.gethostname())
+    __port__ = 12345
+    __s__ = None
+    __value__ = None
+
+    def __init__(self, hostname, portname):
+        self.host = hostname
+        self.port = portname
+        # error checking
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.host, self.port))
+        self.s.listen(5)
+
+    def __del__(self):
+        self.close()
+
+    def accept(self):
+        c, addr = self.s.accept()
+        resp = c.recv(4096)
+        c.close()
+        jresp = json.loads(resp)
+        return jresp
+
+    def getHost(self):
+        return self.host
+
+    def getPort(self):
+        return self.port
+
+    def close(self):
+        self.s.close()
+
+    # send a message without a file, if succeed return True, else return False
+    def sendMessage(self, pyDict):
+        aimHost = pyDict['host']
+        aimPort = pyDict['port']
+        self.value = json.dumps(pyDict)
+        if len(self.value) > 4096:
+            return False
+        else:
+            c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            c.connect((aimHost, aimPort))
+            flag = c.sendall(self.value)
+            if flag == None:
+                return True
+            else:
+                return False
+
+
+
+
